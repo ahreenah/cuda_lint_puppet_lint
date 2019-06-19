deb6948addcd733fbf19d9d71f4bbb2a3f633395
medvosa <38169798+medvosa@users.noreply.github.com>
2019-06-19 15:00:24 +0300

    Add files via upload

---
+++ linter.py
@@ -0,0 +1,44 @@
+"""This module exports the Perl -c util."""
+
+from cuda_lint import Linter, util
+from cudatext import * 
+
+
+class PuppetLint(Linter):
+
+    """Provides an interface to puppet-lint"""
+    cmd = None
+    executable = 'puppet-lint'
+    multiline = False
+    syntax = ('Puppet')
+    regex = (
+             r'^(?P<line>\d+):(?P<col>\d+):'
+             r'((?P<warning>warning)|(?P<error>error)):'
+             r'(?P<message>.+)'
+            )
+    base_cmd = ('--log-format '
+                '%{line}:%{column}:%{kind}:%{message} '
+                '*')
+    tempfile_suffix = 'rb'
+    error_stream = util.STREAM_STDOUT
+    word_re = r'^(".*?"|[-\w]+)'
+
+
+    def split_match(self, match):
+        print('plit_match')
+        print(match)
+   
+        """Return the components of the error."""
+        split_match = super(PuppetLint, self).split_match(match)
+
+        match, line, col, error, warning, message, near = split_match
+        return match, line, 0, '', '', message, ''
+        
+
+
+    def cmd(self):
+        print('cmd')
+        """Return the command line to execute."""
+        result = self.executable + ' ' + self.base_cmd
+
+        return result
---
+++ readme/readme.md
@@ -0,0 +1,13 @@
+Linter for CudaLint plugin.
+It adds support for Puppet lexer.
+It uses "puppet-lint".
+
+'puppet-lint' must be in your system PATH.
+sudo apt-install puppet-lint
+
+Author: Medvosa
+
+License: MIT
+
+Used source from: https://github.com/dschaaff/SublimeLinter-puppet-lint/
+Author: dschaaff
